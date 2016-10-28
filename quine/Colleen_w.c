8e3eff6e0fa899b4889f8d47d4013b1b7091983b
U-AZHRAN\Azhran <firefrai@gmail.com>
2016-10-28 11:56:48 +0200

    transfert quine

---
+++ quine/Colleen_w.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+bob
+*/
+int main(void){char*s="#include <stdio.h>%c/*%cbob%c*/%cint main(void){char*s=%c%s%c;printf(s,10,10,10,10,34,s,34,10,10,10,10);f();}%c/*%cfrancois%c*/%cint f(void){return(0);}%c";printf(s,10,10,10,10,34,s,34,10,10,10,10);f();}
+/*
+francois
+*/
+int f(void){return(0);}
---
+++ quine/Grace_w.c
@@ -0,0 +1,8 @@
+/*
+george
+*/
+#include <stdio.h>
+#define FD FILE *fd=fopen("Grace_kid.c", "w");
+#define S char *s = "/*%cgeorge%c*/%c#include <stdio.h>%c#define FD FILE *fd=fopen(%cGrace_kid.c%c, %cw%c);%c#define S char *s = %c%s%c;%c#define FT int main(void){FD S fprintf(fd, s, 10, 10, 10, 10, 34, 34, 34, 34, 10, 34, s, 34, 10, 10, 10);}%cFT%c";
+#define FT int main(void){FD S fprintf(fd, s, 10, 10, 10, 10, 34, 34, 34, 34, 10, 34, s, 34, 10, 10, 10);}
+FT
